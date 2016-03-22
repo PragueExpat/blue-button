7d7a55375bc4277b1852060f098b5349041dd118
Richard Hensel <richhensel@gmail.com>
2016-03-22 20:20:26 +0100

    epic table cell 8

--- example/example.js
+++ example/example.js
@@ -10,7 +10,7 @@ var doc = bb.xml(data);
 
 //here are result of parsing
 console.log(doc.errors);
-console.log(doc.toString());
+//console.log(doc.toString());
 
 //get document type (e.g. CCDA) of parsed document
 var type = bb.senseXml(doc);
@@ -23,10 +23,10 @@ console.log(sense);
 //convert Xml document into JSON
 var result = bb.parseXml(doc);
 console.log(JSON.stringify(result, null, 4));
-console.log(result);
+//console.log(result);
 
 //convert string into JSON
-result = bb.parseString(data);
-console.log(JSON.stringify(result, null, 4));
+//result = bb.parseString(data);
+//console.log(JSON.stringify(result, null, 4));
 
 // result = // {“data”:“JSON model compliant data here…”, “meta”:{“version”:“1.0.0”,“type”:“CCDA”, … some other metadata}}
